aea71cec5dd4cb820b163d3a1d83f3ce08aa5163
Vignesh_Mayilsamy <vigneshmayilsamy@gmail.com>
2022-11-18 19:23:14 +0530

    Linear search accepted

---
+++ OOPs/linear_search.py
@@ -0,0 +1,26 @@
+pos=-1
+def search(lst,n):
+
+    for i in range(len(lst)):
+        if lst[i]==n:
+            globals()['pos']=i
+            return True
+
+    '''i=0
+
+    while i< len(lst):
+        if lst[i]==n:
+            globals()['pos']=i
+            return True
+        i+=1
+'''
+
+
+lst=[2,14,15,36,46,76,87]
+n=int(input('Enter Num to search:'))
+
+
+if search(lst,n):
+    print('Found',pos+1)
+else:
+    print('Not found')
